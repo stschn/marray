731f19fa7b503285d225122fa2a0a939a6e843c4
stschn <66440117+stschn@users.noreply.github.com>
2023-03-31 20:58:15 +0200

    Update squeeze

--- R/shape.r
+++ R/shape.r
@@ -294,7 +294,7 @@ expand_dims <- function(a, axis = -1L) {
 #' @description Compress the shape of an array by removing singleton dimensions.
 #'
 #' @param a An array.
-#' @param axis The dimensions which should be removed. If \code{NULL} (default), all dimensions of length one are removed.
+#' @param axis The axis or axes which should be removed. If \code{NULL} (default), all axes of length one are removed.
 #' @param order The order in which elements of data should be read during rearrangement after removing of corresponding dimensions.
 #'   By default, the order is equivalent to the \code{C}-style ordering and means elements should be read in row-major order.
 #'   In opposite, the \code{Fortran}-style ordering means elements should be read in column-major order.
@@ -303,17 +303,19 @@ expand_dims <- function(a, axis = -1L) {
 #'   The base R function \code{\link{drop}} does the same as this function. In opposite to \code{drop} this function
 #'   allows reordering the elements of the newly created array as well as specifying only certain axes.
 #'
-#' @return The array \code{a} usually without dimensions of length one.
+#' @return The array \code{a} usually without axes of length one.
 #'
 #' @seealso \code{\link{drop}}, \code{\link{expand_dims}}.
 #'
 #' @export
 squeeze <- function(a, axis = NULL, order = c("C", "F")) {
   order <- match.arg(order)
-  d <- dim(a)
+  a <- .standardize_array(a)
+  d <- DIM(a)
   if (is.null(axis)) {
     newdim <- d[!d %in% c(1L)]
   } else {
+    axis <- .standardize_axis(axis, ndim(a))
     axis1 <- which(d %in% c(1L))
     remove_axis <- axis1[axis1 %in% axis]
     if (isFALSE((is.integer(remove_axis)) && (length(remove_axis) == 0L))) # check for integer (empty)
