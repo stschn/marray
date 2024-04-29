9b53917635437eae8b1ec2c0a19490678764a189
stschn <66440117+stschn@users.noreply.github.com>
2024-04-29 14:50:17 +0200

    Upgrade random

--- R/create.r
+++ R/create.r
@@ -153,39 +153,3 @@ full <- function(dim = NULL, fill_value = NA, dimnames = NULL, order = c("C", "F
 full_like <- function(a, fill_value = NA, order = c("C", "F")) {
   full(dim = DIM(a), fill_value = fill_value, dimnames = dimnames(a), order = order)
 }
-
-#' @rdname random_array
-#' @title Array creation
-#' @description Create array filled with random integer or double values.
-#'
-#' @param dim Shape of the new array.
-#' @param min,max Lower and upper limits of the generated random values.
-#' @param dimnames Either \code{NULL} or the names of the dimensions. This must be a list with one component for each dimension, either \code{NULL} or a character vector of the length given by \code{dim} for that dimension.
-#' @param order Whether to store multidimensional array in C- or Fortran-contiguous (row- or column-wise) order.
-#'
-#' @details This function corresponds roughly to \code{random.rand()} from NumPy (\href{https://numpy.org/doc/stable/reference/random/generated/numpy.random.rand.html}{see}).
-#' @return An array with random values with the given shape.
-#'
-#' @export
-random_int <- function(dim = NULL, min = 0, max = 1, dimnames = NULL, order = c("C", "F")) {
-  marray(sample(x = c(min:max), size = prod(dim), replace = TRUE), dim = dim, dimnames = dimnames, order = order)
-}
-
-#' @rdname random_array
-#' @param a An array whose shape is taken for the result array.
-#' @export
-random_int_like <- function(a, min = 0, max = 1, dimnames = NULL, order = c("C", "F")) {
-  random_int(dim = DIM(a), min = min, max = max, dimnames = dimnames, order = order)
-}
-
-#' @rdname random_array
-#' @export
-random_dbl <- function(dim = NULL, min = 0, max = 1, dimnames = NULL, order = c("C", "F")) {
-  marray(runif(n = prod(dim), min = min, max = max), dim = dim, dimnames = dimnames, order = order)
-}
-
-#' @rdname random_array
-#' @export
-random_dbl_like <- function(a, min = 0, max = 1, dimnames = NULL, order = c("C", "F")) {
-  random_dbl(dim = DIM(a), min = min, max = max, dimnames = dimnames, order = order)
-}
